d5e64ef7a5c4cf68f363f04b2c84586b67877949
Surya <ksurya2007kumar@gmail.com>
2026-06-04 15:40:26 +0530

    javascript varibales,data types and operators done

---
+++ Javascript/script.js
@@ -0,0 +1,85 @@
+// var a = 10;
+// let b = 20;
+// const c = 30;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// var num=10;
+// var str="hello";
+// var undifine;
+// var Null = null;
+// var boolean = true;
+
+// console.log(typeof num);
+// console.log(typeof str);
+// console.log(typeof undefine);
+// console.log(typeof Null);
+// console.log(typeof boolean);
+
+
+// var bigInt=123n;
+//  var symbol=Symbol('li');
+
+//  console.log(typeof bigInt);
+//  console.log(typeof symbol);
+
+//  var arr =[10,20,30,40];
+//  console.log(typeof arr,arr);
+//  var obj={
+//     name:"surya",
+//     dept:["AI&DS","CT"]
+//  }
+//  console.log(typeof obj,obj)
+
+
+// var a = 20;
+// var b = 2;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a % b);
+// console.log(a / b);
+
+// var a = true
+// var b = false
+
+// console.log(a&&b);
+// console.log(a||b);
+// console.log(!a);
+
+// a = 10;
+// b = 20;
+// console.log(a>b);
+// console.log(a<b);
+// console.log(a>=b);
+// console.log(a>=b);
+// console.log(a<=b);
+// console.log(a==b);
+// console.log(a!=b);
+// console.log(a===b);
+// console.log(a!==b);
+// a=10;
+// b=20;
+// a+=b;
+// console.log(a);
+// a-=b;
+// console.log(a);
+// a*=b;
+// console.log(a);
+// a%=b;
+// console.log(a);
+// a/=b;
+// console.log(a)
+// var a = 20
+// console.log(a++);
+// console.log(++a);
+// var a =30;
+// console.log(a--);
+// console.log(--a);
+// a=1;
+// b=1;
+// c=0;
+// var result= a++ + --b + ++c - ++a + ++b + c++ + b++ + a;
+// console.log(result)
